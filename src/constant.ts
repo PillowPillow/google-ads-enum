ac1aae79343f9315426f11050a07a0238e98b1b3
PillowPillow <gaignoux.nicolas@gmail.com>
2019-04-10 17:15:52 +0200

    revert

--- src/constant.ts
+++ src/constant.ts
@@ -7,4 +7,13 @@ export const GoogleAdsVersion = 'v' + pkg.version.split('.')[0];
 export const GoogleAdsWsdlUrl = 'https://adwords.google.com/api/adwords/cm/';
 export const GenDirPath = Path.join(__dirname, '..', 'lib');
 
-export const WSDLEndpoints = ['AccountLabelService', 'AdCustomizerFeedService', 'AdGroupAdService', 'AdGroupBidModifierService', 'AdGroupCriterionService', 'AdGroupExtensionSettingService', 'AdGroupFeedService', 'AdGroupService', 'AdParamService', 'AdService', 'AdwordsUserListService', 'AssetService', 'BatchJobService', 'BiddingStrategyService', 'BudgetOrderService', 'BudgetService', 'CampaignBidModifierService', 'CampaignCriterionService', 'CampaignExtensionSettingService', 'CampaignFeedService', 'CampaignGroupPerformanceTargetService', 'CampaignGroupService', 'CampaignService', 'CampaignSharedSetService', 'ConstantDataService', 'ConversionTrackerService', 'CustomAffinityService', 'CustomerExtensionSettingService', 'CustomerFeedService', 'CustomerNegativeCriterionService', 'CustomerService', 'CustomerSyncService', 'DataService', 'DraftAsyncErrorService', 'DraftService', 'FeedItemService', 'FeedItemTargetService', 'FeedMappingService', 'FeedService', 'LabelService', 'LocationCriterionService', 'ManagedCustomerService', 'MediaService', 'OfflineCallConversionFeedService', 'OfflineConversionAdjustmentFeedService', 'OfflineConversionFeedService', 'OfflineDataUploadService', 'ReportDefinitionService', 'SharedCriterionService', 'SharedSetService', 'TargetingIdeaService', 'TrafficEstimatorService', 'TrialAsyncErrorService', 'TrialService'];
+export const WSDLEndpoints = [
+	'CampaignService',
+	'AdGroupService',
+	'AdGroupAdService',
+	'CampaignCriterionService',
+	'CampaignBidModifierService',
+	'AdGroupBidModifierService',
+	'AdGroupCriterionService',
+	'AdService'
+];
