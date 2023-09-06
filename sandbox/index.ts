b936062b939a0986d61b5cc6620e58a3dbcde5dd
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2023-09-06 11:42:24 +0200

    increase target version

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -1,3 +1,3 @@
-import { AccessReason } from '../lib/index';
+import { AccessReason, ContentLabelType } from '../lib/index';
 
-console.log(AccessReason.LICENSED);
+console.log(ContentLabelType.LIVE_STREAMING_VIDEO);
